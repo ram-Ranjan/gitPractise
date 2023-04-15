d58304fde5989704b9680de26260f2af285c02c5
ram-Ranjan <ranjanirl3107@gmail.com>
2023-04-15 12:54:34 +0530

    first file second commit

--- sample.java
+++ sample.java
@@ -3,6 +3,9 @@ class Sample{
     public static void main(String [] args)
     {
         System.out.println("Hello Github");
+
+                System.out.println("Second Commit");
+
     }
    
 }
